1aabdd47626b1c5241ca2737524e79d98905d499
Kalyan Kumar Devarakonda <59095784+Kalayankk@users.noreply.github.com>
2019-12-20 23:03:15 +0530

    initial file

---
+++ Passorfail.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Posorneg
+{
+ public static void main(String[] args)
+ {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("enter the number:");
+  int a=sc.nextInt();
+  if (a>0&&a=0)
+  System.out.println("entered number is +ve:");
+  else
+  System.out.println("entered number is -ve");
+ }
+}
